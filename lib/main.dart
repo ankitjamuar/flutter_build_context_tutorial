ca655b91159fac2b44f8b1912409175933c43473
Ankit Sinha <ankit@Ankits-MacBook-Pro.local>
2021-05-30 10:11:52 +0530

    Wrapped with 2 Builder Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,35 +5,21 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context1) {
-    print(context1.findRenderObject());
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'BuildContext Demo',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Build Context"),
-        ),
         body: Center(
-          child: Container()
-        ),
-        floatingActionButton: Builder(
-          builder: (context2) =>  Builder(
-            builder: (context3)  {
-              print(context1.findRenderObject());
-              return FloatingActionButton(
-              onPressed: (){
-                Scaffold.of(context2).showSnackBar(
-                    new SnackBar(content: Text("MeshCookie"))
-                );
-              },
-              tooltip: 'Increment',
-              child: Icon(Icons.add),
-            );},
+          child: Builder(
+            builder: (context2) => Builder(
+              builder: (context3) => ElevatedButton(
+                  onPressed: () {
+                    Scaffold.of(context3).showSnackBar(
+                        new SnackBar(content: Text("MeshCookie")));
+                  },
+                  child: Text("Open Snackbar")),
+            ),
           ),
         ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
